a617c63713cfb2cf2fb45a9f0855aad3af616b63
Eric <eric_kotzman1@my.vcccd.edu>
2025-11-08 07:57:35 +0000

    Fixed gunicorn venv issue and permissions on flask service traversal

--- simple_http_server.py
+++ simple_http_server.py
@@ -1,26 +1,21 @@
 ## Simple HTTP Server project
 
 from flask import Flask, render_template
+import json
+
+
 app = Flask(__name__)
 
-
-project_list = [
-    {"name": "Mini HTTP Server", "description": "A simple Flask-based web sever.", "url": "https://github.com/PassTheSalt-1/Root"},
-    {"name": "System Info Script", "description": "Collects system info using Bash.", "url": "https://github.com/PassTheSalt-1/Root"},
-    {"name": "Azure Funtions Demo", "description": "Explores serverless app integration.", "url": "https://github.com/PassTheSalt-1/Root"}
-]
-
+def load_projects():
+    with open("projects.json", "r") as f:
+        return json.load(f)
 
 ##Home page
 @app.route('/')
 def home():
+    projects = load_projects()
 
-    page_title = "Home - My Flask App"
-    author = "ME"
-
-
-
-    return render_template("index.html", title="Home - My Flask App", author="ME", projects=project_list )
+    return render_template("index.html", title="Home - My Flask App", author="ME", projects=projects)
 
 #about page
 @app.route('/about')
@@ -29,7 +24,8 @@ def about():
 
 @app.route('/projects')
 def projects():
-    return render_template("projects.html", title="Projects - My Flask App", projects=project_list)
+    project_data = load_projects()
+    return render_template("projects.html", title="Projects - My Flask App", projects=project_data)
 
 
 if __name__ == '__main__':
